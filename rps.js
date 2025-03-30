24611eb06000f06982f6fbdcb685489fe2c09058
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-03-30 01:32:09 -0500

    Created the function that plays a round and chooses a winner after comparing both choices.

--- rps.js
+++ rps.js
@@ -11,15 +11,42 @@ function getComputerChoice() {
         choice = "Scissors";
     }
     //Return the choice//
-    return choice;
+    return choice.toUpperCase();
 }
 
 function getHumanChoice() {
     //Prompts user for pick//
     let choice = prompt("What's your choice?:");
     //Return the choice//
-    return choice;
+    return choice.toUpperCase();
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    
+    console.log(`You threw: ${humanChoice}`);
+    console.log(`The computer threw: ${computerChoice}`);
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+    } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
+        console.log(`Nice! ${humanChoice} beats ${computerChoice}!`);
+    } else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
+        console.log(`Nice! ${humanChoice} beats ${computerChoice}!`);
+    } else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+        console.log(`Nice! ${humanChoice} beats ${computerChoice}!`);
+    } else if (humanChoice == "ROCK" && computerChoice == "PAPER") {
+        console.log(`Oops! ${computerChoice} beats ${humanChoice}!`);
+    } else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
+        console.log(`Oops! ${computerChoice} beats ${humanChoice}!`);
+    } else {
+        console.log(`Oops! ${computerChoice} beats ${humanChoice}!`);
+    } 
+}
+
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
